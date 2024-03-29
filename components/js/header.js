e4245c4a456e347da0368850e27a77e305a4bb02
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-03-29 17:20:51 +0300

    add index.2 html and style2.css

--- components/js/header.js
+++ components/js/header.js
@@ -3,6 +3,12 @@ const template = document.createElement(`template`)
 template.innerHTML = `
 <menu>
     <ul class="ul-header">
+        <li>
+            <div class="logo">
+                <img src="components/img/logo.svg" alt="">
+                E-Mentor
+            </div>
+        </li>
         <li><a class="a-header" href="#">О&nbspнас</a></li>
         <li><a class="a-header" href="#">Платформа</a></li>
         <li><a class="a-header" href="#">Курсы</a></li>
