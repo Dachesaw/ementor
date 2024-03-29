36abf3b1a13372bf8a87e5eef8e003b795a7ed16
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-03-29 03:18:10 +0300

    add menu style and svg content

--- components/js/header.js
+++ components/js/header.js
@@ -1,17 +1,17 @@
 const template = document.createElement(`template`)
 
 template.innerHTML = `
-    <menu>
-        <ul>
-            <li><a href="#">О нас</a></li>
-            <li><a href="#">Платформа</a></li>
-            <li><a href="#">Курсы</a></li>
-            <li><a href="#">Лекции</a></li>
-            <li><a href="#">Тарифы</a></li>
-            <li><a href="#">Блог</a></li>
-            <li><a href="#">Контакты</a></li>
-        </ul>
-    </menu>
+<menu>
+    <ul class="ul-header">
+        <li><a class="a-header" href="#">О нас</a></li>
+        <li><a class="a-header" href="#">Платформа</a></li>
+        <li><a class="a-header" href="#">Курсы</a></li>
+        <li><a class="a-header" href="#">Лекции</a></li>
+        <li><a class="a-header" href="#">Тарифы</a></li>
+        <li><a class="a-header" href="#">Блог</a></li>
+        <li><a class="a-header" href="#">Контакты</a></li>
+    </ul>
+</menu>
     `
 
 document.body.appendChild(template.content)
